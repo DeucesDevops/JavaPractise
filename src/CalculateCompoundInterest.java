ae98d1d46af8d9e7a9c1f3a172e9d598e87a0ed6
DeucesDevops <macroni607@gmail.com>
2025-03-10 18:53:25 +0000

    Compound Interest project

---
+++ src/CalculateCompoundInterest.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class CalculateCompoundInterest {
+    public static void compoundInterest(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        double principal;
+        double rate;
+        int numberOfTimesCompounded;
+        int years;
+        double totalAmount;
+
+        System.out.print("Enter the principal: ");
+        principal = scanner.nextDouble();
+
+        System.out.print("Enter the rate: ");
+        rate = scanner.nextDouble() / 100;
+
+        System.out.print("Enter the number of times the interest is compounded per year: ");
+        numberOfTimesCompounded = scanner.nextInt();
+
+        System.out.print("Enter the number of years: ");
+        years = scanner.nextInt();
+
+        totalAmount = principal * Math.pow(1 + rate / numberOfTimesCompounded, numberOfTimesCompounded * years);
+
+        System.out.printf("The amount after %d years is £%.2f ", years, totalAmount);
+
+
+
+        scanner.close();
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,12 @@
 
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args){
-        //Calculate the area of a triangle
 
-//        AreaOfTriangle   triangleArea = new AreaOfTriangle();
-//
-//        triangleArea.triangleArea();
+    public static void main(String[] args) {
+        CalculateCompoundInterest calculateCompoundInterest = new CalculateCompoundInterest();
 
-        //shopping cart programme
-        ShoppingCartProgramme shoppingCart = new ShoppingCartProgramme();
-
-        shoppingCart.calculator();
+        calculateCompoundInterest.compoundInterest(args);
     }
 }
